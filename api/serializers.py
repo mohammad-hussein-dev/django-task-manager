36cf573364d4b9b26d575ae19f48bb57e751b5d4
Mohammad Hossein <YOUR_EMAIL>
2026-07-16 23:14:20 +0330

    Add TaskSerializer for REST API

---
+++ api/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+
+from tasks.models import Task
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    """Serializer for Task model."""
+
+    class Meta:
+        model = Task
+        fields = "__all__"
+        read_only_fields = (
+            "id",
+            "created_at",
+            "updated_at",
+        )
